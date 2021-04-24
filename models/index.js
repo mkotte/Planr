9bf7cce06c6299c1f12a57e961ea4e816c52979f
mkotte <74936870+mkotte@users.noreply.github.com>
2021-04-24 14:09:36 -0400

    starter files

---
+++ models/index.js
@@ -0,0 +1,5 @@
+// TODO: create, then import models required here
+
+// TODO: declare associations here
+
+// TODO: export module here
---
+++ routes/api/index.js
@@ -0,0 +1,12 @@
+const router = require('express').Router();
+
+// TODO: Create specific routes here
+// const commentRoutes = require('./comment-routes');
+// const postRoutes = require('./post-routes');
+// const userRoutes = require('./user-routes');
+
+// router.use('/comments', commentRoutes);
+// router.use('/posts', postRoutes);
+// router.use('/users', userRoutes);
+
+module.exports = router;
