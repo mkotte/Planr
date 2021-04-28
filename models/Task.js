17ccb7d8c6f6755417bb8c0118394924d8bb2104
Schimmoellerm <schimmoellerm@gmail.com>
2021-04-27 20:40:42 -0400

    created Task model

---
+++ models/Task.js
@@ -0,0 +1,51 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Task extends Model {}
+
+Task.init(
+    {
+        // * ID (PK)
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        // * Title
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        // * Comments/Descriptions
+        description: {
+            type: DataTypes.STRING,
+            allowNull: true,
+        },
+        // * Positions (x + y needed integer)
+        positionX: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        positionY: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        // * Card ID (FK)
+        card_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'card',
+                key: 'id',
+            },
+        },
+    },
+    {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'task',
+    }
+);
+
+module.exports = Task;
