361acbe8ce0995e4c90a457bcd0bcddcf20928b0
mkotte <74936870+mkotte@users.noreply.github.com>
2021-04-28 02:27:25 -0400

    retouched route's
    added comments in index.js to help jesse figure out what its doing / how to properly format it

--- controllers/api/boardRoutes.js
+++ controllers/api/boardRoutes.js
@@ -47,7 +47,7 @@ router.put('/:id', async(req, res) => {
     }
     res.status(200).json({message:'Successfully updated'})
   } catch (err){
-    res.status(404).json(err)
+    res.status(500).json(err)
   }
 });
 
--- controllers/api/index.js
+++ controllers/api/index.js
@@ -5,6 +5,11 @@ const cardRoutes = require('./cardRoutes');
 const boardRoutes = require('./boardRoutes');
 const taskRoutes = require('./taskRoutes');
 
+// TODO: Let Jesse fix
+// router.use is declaring what route to use
+// the first paramater is where we are defining our api endpoints (i.e. api/variable-goes-here)
+// the second paramater is where we are pulling the information routes for the api called 
+// (where are we already requiring the files including the specific routing information?)
 router.use('/cardRoutes', cardData);
 router.use('/boardRoutes', boardData);
 router.use('/taskRoutes', taskData);
