7d16e57e4bb015d7bbded3f081d885c6f26034a2
mkotte <74936870+mkotte@users.noreply.github.com>
2021-04-28 02:05:41 -0400

    updated board.html and drag-n-drop.js

--- public/assets/js/drag-n-drop.js
+++ public/assets/js/drag-n-drop.js
@@ -1,7 +1,10 @@
 // TODO: determine best methods for file structure and then implement drag n drop function's
+const example2Left = document.querySelector('#example2-left')
+const example2Right = document.querySelector('#example2-right')
 
-// Simple list
-Sortable.create(simpleList, { //Options go here
+new Sortable(example2Left, {
+    group: 'shared', // set both lists to same group
+    animation: 150,
     onEnd: function (/**Event*/evt) {
 		var itemEl = evt.item;  // dragged HTMLElement
 		evt.to;    // target list
@@ -13,11 +16,22 @@ Sortable.create(simpleList, { //Options go here
 		evt.clone // the clone element
 		evt.pullMode;  // when item is in another sortable: `"clone"` if cloning, `true` if moving
         console.log(evt.newIndex)
-    }});
-
-// List with handle
-Sortable.create(listWithHandle, {
-  handle: '.glyphicon-move',
-  animation: 150
+    }
 });
 
+new Sortable(example2Right, {
+    group: 'shared',
+    animation: 150,
+    onEnd: function (/**Event*/evt) {
+		var itemEl = evt.item;  // dragged HTMLElement
+		evt.to;    // target list
+		evt.from;  // previous list
+		evt.oldIndex;  // element's old index within old parent
+		evt.newIndex;  // element's new index within new parent
+		evt.oldDraggableIndex; // element's old index within old parent, only counting draggable elements
+		evt.newDraggableIndex; // element's new index within new parent, only counting draggable elements
+		evt.clone // the clone element
+		evt.pullMode;  // when item is in another sortable: `"clone"` if cloning, `true` if moving
+        console.log(evt.newIndex)
+    }
+});
