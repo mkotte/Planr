7fe178773514a41375bf62b28b5bf34198cb38bc
Schimmoellerm <schimmoellerm@gmail.com>
2021-04-29 21:25:16 -0400

    changed name to boardName

--- db/seeds/board_seed.sql
+++ db/seeds/board_seed.sql
@@ -2,10 +2,10 @@ USE project_db
 
 CREATE TABLE board (
     id INT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(100) NOT NULL,
+    boardName VARCHAR(100) NOT NULL,
     userID VARCHAR(100) NOT NULL,
     PRIMARY KEY  (id)
 );
 
-INSERT INTO board (id, name, userID)
+INSERT INTO board (id, boardName, userID)
 VALUES (0, "Name0", "User0"), (1, "Name0", "User0"), (2, "Name0", "User0");
