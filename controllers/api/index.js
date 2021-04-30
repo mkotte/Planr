c113f4994c058ba031f6194ff0317b1db265306a
Jesse Rutter <rutter1010@gmail.com>
2021-04-29 20:56:31 -0400

    tasks + users seeds written

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -5,12 +5,6 @@ const cardRoutes = require('./cardRoutes');
 const boardRoutes = require('./boardRoutes');
 const taskRoutes = require('./taskRoutes');
 
-// TODO: Let Jesse fix
-// router.use is declaring what route to use
-// the first paramater is where we are defining our api endpoints (i.e. api/variable-goes-here)
-// the second paramater is where we are pulling the information routes for the api called 
-// (where are we already requiring the files including the specific routing information?)
-
 router.use('/api/cards', cardRoutes);
 router.use('/api/boards', boardRoutes);
 router.use('/api/tasks', taskRoutes);
