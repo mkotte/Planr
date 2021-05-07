739dd0f66b892ce9fb353aa1fa750c4fa0f2741c
Schimmoellerm <schimmoellerm@gmail.com>
2021-05-06 21:09:43 -0400

    login/auth working

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -24,17 +24,19 @@ router.get('/', async (req, res) => {
 
 router.post('/login', async (req, res) => {
     try {
-        const userData = await User.findOne({ where: { email: req.body.email } });
+        const user = await User.findOne({ where: { email: req.body.email } });
 
-        if (!userData) {
+        
+        if (!user) {
             res
                 .status(400)
                 .json({ message: 'Incorrect email or password, please try again' });
             return;
         }
 
-        const validPassword = await userData.checkPassword(req.body.password);
-
+        const validPassword = user.checkPassword(req.body.password);
+        console.log(req.body.password)
+        console.log(validPassword);
         if (!validPassword) {
             res
                 .status(400)
@@ -43,10 +45,10 @@ router.post('/login', async (req, res) => {
         }
 
         req.session.save(() => {
-            req.session.user_id = userData.isSoftDeleted;
+            req.session.user_id = user.isSoftDeleted;
             req.session.logged_in = true;
 
-            res.json({ user: userData, message: 'You are now logged in!' });
+            res.json({ user: user, message: 'You are now logged in!' });
         });
 
         }catch (err) {
